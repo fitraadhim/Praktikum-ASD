60df8acf42e8534869119e6b236e569bd4000814
fitraadhim <azzazria123@gmail.com>
2025-05-15 01:42:01 +0700

    Add files via upload

---
+++ Jobsheet 9/Mahasiswa19.java
@@ -0,0 +1,22 @@
+public class Mahasiswa19 {
+    String nim;
+    String nama;
+    String kelas;
+    int nilai;
+    
+    Mahasiswa19(){
+
+    }
+    
+    Mahasiswa19(String nama, String nim, String kelas){
+        this.nama = nama;
+        this.nim = nim;
+        this.kelas = kelas;
+        nilai = -1;
+
+    }
+    void tugasDinilai(int nilai){
+        this.nilai = nilai;
+    }
+
+}
